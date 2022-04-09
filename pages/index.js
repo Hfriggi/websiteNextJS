61d24cf4a52e0f0122330cf5fce8b0ad485b812e
Hfriggi <henriquefriggi1@gmail.com>
2022-04-09 00:12:39 -0300

    FirstPage

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,20 @@
+import Head from 'next/head'
+
 function Home(){
-    return <div>Home My WebSite!</div>
+    return (
+    <div>
+        <Head>
+            <meta charSet='utf-8' />
+            <meta name="description" content="Site-portifólio em desenvolvimento"/>
+            <meta name="author" content="Henrique Friggi"/>
+            <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+            <title> Henrque Friggi WebSite</title>
+        </Head>
+        My WebSite!
+        <head2>
+             Site in development!
+        </head2>
+    </div>)
 }
 
 export default Home
