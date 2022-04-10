ef9602e7cd84bd6613c95e59be17277293d758a6
Hfriggi <henriquefriggi1@gmail.com>
2022-04-10 17:35:26 -0300

    Red menu

--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,33 @@
+import { Html } from 'next/document'
 import Head from 'next/head'
 
 function Home(){
     return (
     <div>
-        <Head>
-            <meta charSet='utf-8' />
-            <meta name="description" content="Site-portifólio em desenvolvimento"/>
-            <meta name="author" content="Henrique Friggi"/>
-            <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-            <title> Henrque Friggi WebSite</title>
-        </Head>
-        Henrique Friggi
-        <h1>
-        My WebSite  in development!
-        </h1>
-    </div>)
-}
+    <head>
+        <meta charSet='UTF-8'></meta>
+        <title>HenriqueFriggi</title>
+    </head>
+    <body>
+        <ul>
+            <li>
+                <a href='#' data-text='&nbsp;Home'>&nbsp;Home</a>
+            </li>
+            <li>
+                <a href='#' data-text='&nbsp;About'>&nbsp;About</a>
+            </li>
+            <li>
+                <a href='#' data-text='&nbsp;Projects'>&nbsp;Projects</a>
+            </li>
+            <li>
+                <a href='#' data-text='&nbsp;Work'>&nbsp;Work</a>
+            </li>
+            <li>
+                <a href='#' data-text='&nbsp;Contact'>&nbsp;Contact</a>
+            </li>
+        </ul>
+    </body>
+    </div>
+)}
 
 export default Home
