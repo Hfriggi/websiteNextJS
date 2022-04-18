85cb91dd549a03a19d5e55b623323d7809a5cbd4
Hfriggi <henriquefriggi1@gmail.com>
2022-04-18 02:32:52 -0300

    Image and text

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import { Html } from 'next/document'
+import Image from 'next/image';
 import React, { Fragment } from 'react';
 import Head from 'next/head'
 import react from 'react'
@@ -15,18 +16,21 @@ function Home(){
         <title>Henrique Friggi WebSite</title>
         <link rel='stylesheet' href='styles.css'></link>
     </head>
-    <div className='body1'>
-        <body>
-            <center>
+    <body>
+        <center>
+            <div className='image'>
+                <img src='/images/perfil.jpg'/>
+            </div>
+            <div className='box'>
                 <h1 className='TextTitle'>
                     A Short Introduction
                 </h1>
                 <a className='TextDescribe'>
-                    I'm a Brazilliam Developer in formation, learning front-end and React Native!
+                Graduating in Control and Automation Engineering and front-end developer in training. Amateur astronomer. Self-taught, motivated and fascinated by technology.
                 </a>
-            </center>
-        </body>
-    </div>
+            </div>
+        </center>
+    </body>
     <Footer />
     </Fragment>
 )
